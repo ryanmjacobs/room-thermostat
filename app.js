9cdeafd8642e114dc125be8e42d5e47b1fb9215e
Ryan Jacobs <ryan@rmj.us>
2018-12-30 15:11:46 -0800

    wrapper

--- app.js
+++ app.js
@@ -54,5 +54,7 @@ function calc_heater_state(state, temp) {
 // init sensor and begin control loop
 heater.get().then(state => {
     console.log("current heater state: " + state);
-    bme280.init().then(() => control_loop(state));
+    bme280.init().then(function() {
+        control_loop(state);
+    });
 });
