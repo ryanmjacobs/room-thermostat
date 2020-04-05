2c9ee0b044c66982e87b33248f6e298817d61204
ryan <ryan@alarmpi>
2020-04-05 16:44:19 -0700

    ;discovery protocol;

--- launch_tmux.sh
+++ launch_tmux.sh
@@ -7,4 +7,5 @@ run() { tmux send-keys -t $sesh "$1" Enter;}
 create() { tmux new-window -t $sesh; run "$1";}
 tmux new -d -s $sesh
 
-run 'while true; do source DUKELANA_PG_CRED; node sample.js; sleep 1; done'
+run 'while sleep 1; do source DUKELANA_PG_CRED; node sample.js; done'
+create 'while sleep 1; do ruby ~/discovery/broadcast.rb; done'
