3b18b0a67e9cff1ff45317ff6b0057a5134158e3
ryan <ryan@alarmpi>
2020-04-05 16:44:47 -0700

    Squashed 'discovery/' content from commit cf5f846
    git-subtree-dir: discovery
    git-subtree-split: cf5f846d8f1c450dce1db24bd54ef10b4446f09b

---
+++ listen.rb
@@ -0,0 +1,33 @@
+require "json"
+require "socket"
+require "colorize"
+
+def netcolor(addr)
+    return :red    if addr.map{|x| x.start_with?("192.168.")}.any?
+    return :yellow if addr.map{|x| x.start_with?("10.0.10.")}.any?
+    return :blue
+end
+ 
+PORT = 31001 
+HOST = ""
+
+socket = UDPSocket.open
+socket.setsockopt(Socket::SOL_SOCKET, Socket::SO_BROADCAST, true)
+
+socket.bind(HOST, PORT)
+while true do
+    begin
+        data = socket.recvfrom(32768)[0]
+        jd   = JSON.parse(data)
+        addr = jd["addr"]
+            .filter{|x| x["ifname"] != "lo" && !x["ifname"].start_with?("docker") && x["operstate"] != "DOWN" && x["addr_info"].length >= 1}
+            .map{|x| x["addr_info"][0]["local"]}
+
+        addr_color  = netcolor(addr)
+        pretty_addr = addr.map{|x| x.ljust(16)}.join(",")
+        puts "#{jd["hostname"].rjust(16).bold.colorize(addr_color)} - #{pretty_addr}"
+    rescue
+        puts jd
+    end
+    STDOUT.flush
+end
