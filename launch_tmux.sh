1fb3676d1df7055ace834fa8cc6e229676afa2fb
ryan <ryan@alarmpi>
2020-04-05 16:45:07 -0700

    use subtree

--- launch_tmux.sh
+++ launch_tmux.sh
@@ -8,4 +8,4 @@ create() { tmux new-window -t $sesh; run "$1";}
 tmux new -d -s $sesh
 
 run 'while sleep 1; do source DUKELANA_PG_CRED; node sample.js; done'
-create 'while sleep 1; do ruby ~/discovery/broadcast.rb; done'
+create 'while sleep 1; do ruby ./discovery/broadcast.rb; done'
